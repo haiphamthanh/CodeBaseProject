0a1db945b4e51ebe5232daebeae686e3f8b18f05
HaiPham <pthai076@icloud.com>
2022-03-12 15:26:20 +0700

    init project

---
+++ CodeBaseProject/CodeBaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBaseProjectApp.swift
+//  CodeBaseProject
+//
+//  Created by HaiKaito on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeBaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBaseProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeBaseProject
+//
+//  Created by HaiKaito on 12/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
