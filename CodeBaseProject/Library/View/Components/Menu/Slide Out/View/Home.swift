481b5decb2d7b8649197cbed17a449ad69948765
HaiPham <pthai076@icloud.com>
2022-04-21 00:58:46 +0700

    [add] home navi bar

--- CodeBaseProject/Library/View/Components/Menu/Slide Out/View/Home.swift
+++ CodeBaseProject/Library/View/Components/Menu/Slide Out/View/Home.swift
@@ -10,10 +10,49 @@ import SwiftUI
 struct Home: View {
 	@Binding var showMenu: Bool
     var body: some View {
-		Button {
-			withAnimation{ showMenu.toggle() }
-		} label: {
-			Text("Show menu")
+		VStack {
+			VStack(spacing: 0) {
+				HStack {
+					Button {
+						withAnimation {
+							showMenu.toggle()
+						}
+					} label: {
+						Image("img_my_avatar")
+							.resizable()
+							.aspectRatio(contentMode: .fill)
+							.frame(width: 35, height: 35)
+							.clipShape(Circle())
+					}
+					
+					Spacer()
+					
+					// Navigation Link...
+					NavigationLink {
+						Text("Time line view")
+							.navigationTitle("Time line")
+					} label: {
+						Image("ic_home")
+							.resizable()
+							.renderingMode(.template)
+							.aspectRatio(contentMode: .fit)
+							.frame(width: 22, height: 22)
+							.foregroundColor(.primary)
+					}
+				}
+				.padding(.horizontal)
+				.padding(.vertical, 10)
+				
+				Divider()
+			}
+			.overlay(
+				Image("ic_setting")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 25, height: 25)
+			)
+			
+			Spacer()
 		}
     }
 }
